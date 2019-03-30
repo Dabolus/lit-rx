9bbedcbb4e7a2ee89036f470b5ba4cf6fdb322b5
Giorgio Garasto <giorgio@garasto.it>
2019-03-30 09:58:08 +0100

    refactor: observable -> subscribable

--- src/observe.ts
+++ src/observe.ts
@@ -3,46 +3,44 @@ import {Subscribable} from 'rxjs';
 
 interface PreviousValue {
   readonly value: unknown;
-  readonly observable: Subscribable<unknown>;
+  readonly subscribable: Subscribable<unknown>;
 }
 
 // For each part, remember the value that was last rendered to the part by the
-// observe directive, and the observable that was last set as a value.
-// The observable is used as a unique key to check if the last value
+// observe directive, and the subscribable that was last set as a value.
+// The subscribable is used as a unique key to check if the last value
 // rendered to the part was with observe. If not, we'll always re-render the
 // value passed to observe.
 const previousValues = new WeakMap<Part, PreviousValue>();
 
 /**
- * A directive that renders the items of an observable[1], replacing
+ * A directive that renders the items of a subscribable, replacing
  * previous values with new values, so that only one value is ever rendered
  * at a time.
  *
- * [1]: http://reactivex.io/documentation/observable.html
- *
- * @param value An observable
+ * @param value A subscribable
  */
 export const observe =
-    directive(<T>(observable: Subscribable<T>) => (part: Part) => {
-      // If we have already set up this observable in this part, we
+    directive(<T>(subscribable: Subscribable<T>) => (part: Part) => {
+      // If we have already set up this subscribable in this part, we
       // don't need to do anything
       const previousValue = previousValues.get(part);
 
       if (previousValue !== undefined &&
-          observable === previousValue.observable) {
+          subscribable === previousValue.subscribable) {
         return;
       }
 
-      observable.subscribe((value) => {
-        // If we have the same value and the same observable in the same part,
+      subscribable.subscribe((value) => {
+        // If we have the same value and the same subscribable in the same part,
         // we don't need to do anything
         if (previousValue !== undefined && part.value === previousValue.value &&
-            observable === previousValue.observable) {
+            subscribable === previousValue.subscribable) {
           return;
         }
 
         part.setValue(value);
         part.commit();
-        previousValues.set(part, {value, observable});
+        previousValues.set(part, {value, subscribable});
       });
     });
